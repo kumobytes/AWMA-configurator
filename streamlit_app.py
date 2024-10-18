0c8ee3ef36db9d93eaa8dbdbdc9a70e233f0c89f
Sophie <sophie.xu@erntec.com.au>
2024-10-18 04:01:33 +0000

    Some formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,15 @@ from data_processing import getBasePrice
 df = pd.read_csv("Door_prices.csv")
 
 st.title("AWMA configurator")
+col1, col2 = st.columns(2)
 
 door_prices = getBasePrice(df)
-
-door_size = st.selectbox("Select Door Size:", list(door_prices.keys()))
-door_type = st.selectbox("Select Door Type:", ["Standard", "Fully Sealed"])
+with col1:
+    door_size = st.selectbox("Select Door Size:", list(door_prices.keys()))
+    st.write(f"Selected Door Size: {door_size}")
+with col2:
+    door_type = st.selectbox("Select Door Type:", ["Standard", "Fully Sealed"])
+    st.write(f"Selected Door Type: {door_type}")
 
 price = door_prices[door_size][door_type]
-
-st.write(f"Selected Door Size: {door_size}")
-st.write(f"Selected Door Type: {door_type}")
-st.write(f"Base Price: ${price}")
+st.write("Base Price:" + price)
