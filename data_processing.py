55fc753e6cc4a9b98710692189a9b1a945722f2a
Sophie <sophie.xu@erntec.com.au>
2024-10-18 05:54:11 +0000

    Started with hardware items (need review)

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("Door_prices.csv")
+df1 = pd.read_csv("Hardware_prices.csv", encoding="latin-1")
 
 def convert_currency(value):
     return float(value.replace('$', '').replace(',', ''))
@@ -11,6 +12,11 @@ def cleanBasePriceData(df):
     df['amount'] = df[' UnitSell '].apply(convert_currency)
     return df
 
+def cleanHWPriceData(df):
+    df = df.loc[:,["Applicable Door Type", "Hardware Type",
+                  "Hardware description"," Unit Sell  "]]
+    df['amount'] = df[' Unit Sell  '].apply(convert_currency)
+    return df
 
 def getBasePrice(df):
     df = cleanBasePriceData(df)
@@ -26,6 +32,22 @@ def getBasePrice(df):
         door_prices[size][door_type] = price
 
     return door_prices
+
+def getHWPrice(df):
+    df = cleanHWPriceData(df)
+    HW_prices = {}
+    for index, row in df.iterrows():
+        hardwareType = row['Hardware Type']
+        price = row[' Unit Sell  ']
+        desc = row['Hardware description']
+        
+        if hardwareType not in HW_prices:
+            HW_prices[hardwareType] = {}
+
+        HW_prices[hardwareType][desc] = price
+    
+    return HW_prices
+
 
 test = getBasePrice(df)
 print(test)
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,43 @@ from data_processing import getBasePrice
 df = pd.read_csv("Door_prices.csv")
 
 st.title("AWMA configurator")
+
+st.write("Step 1: Select type of door and size")
 col1, col2 = st.columns(2)
 
 door_prices = getBasePrice(df)
 with col1:
+    door_type = st.radio("Select Door Type:", ["Standard", "Fully Sealed"], horizontal = True)
+    st.write(f"Selected Door Type: {door_type}")
+with col2:
     door_size = st.selectbox("Select Door Size:", list(door_prices.keys()))
     st.write(f"Selected Door Size: {door_size}")
-with col2:
-    door_type = st.selectbox("Select Door Type:", ["Standard", "Fully Sealed"])
-    st.write(f"Selected Door Type: {door_type}")
 
 price = door_prices[door_size][door_type]
-st.write("Base Price:" + price)
+#st.write("Base Price:" + price)
+
+st.divider()
+
+st.write("Step 2: Select hinge side")
+hinge_type = st.radio("Select Hinge Type:", ["Left hinge", "Right hinge"], horizontal = True)
+
+st.divider()
+
+st.write("Step 3: Select hardware required")
+
+if door_type in door_prices[door_size]:
+    # Add conditional hardware options based on door type
+    if door_type == "Standard":
+        mortice = st.selectbox("Mortice:", ["Universal Mortice Lock 3772TSC", 
+                                            "Australian Oval Cylinder"])
+        latchPlate = st.selectbox("Latch Plate:",["Standard Erntec Latch",
+                                                  "Striker, Electric, 12-30Vdc, 25kg Pre-Load, Multi-function, No-Lip"])
+
+
+    elif door_type == "Fully Sealed":
+        mortice = st.selectbox("Mortice:", ["Universal Mortice Lock 3772TSC", "Australian Oval Cylinder"])
+        latchPlate = st.selectbox("Latch Plate:",["Standard Erntec Latch FS",
+                                                  "Striker, Electric, 12-30Vdc, 25kg Pre-Load, Multi-function, No-Lip"])
+
+holder = ["Custom Latch Block:", "Exterior Plate/ Handle:", "Interior Plate/ Handle:", "Additional Hardware:"]
+
